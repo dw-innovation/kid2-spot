74fce8d1f30d82f22d3538e8bc5c4d9e20f1e4a9
Ipek Baris Schlicht <ipek.baris-schlicht@dw.com>
2024-02-22 09:25:32 +0100

    update data

--- scripts/scripts/convert_tags_to_imr.sh
+++ scripts/scripts/convert_tags_to_imr.sh
@@ -1,3 +1,3 @@
 python -m datageneration.tags_to_imr \
---tag_list_path datageneration/data/Tag_List_v10.csv \
---output_file datageneration/results/IMR_Dataset_v12/imr-tag-db_v2.jsonl
+--tag_list_path datageneration/data/Tag_List_v11.csv \
+--output_file datageneration/results/v12/imr-tag-db_v3.jsonl
