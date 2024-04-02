888514fbdb341ca3e58c8d21d88e719dba18a36a
Ipek Baris Schlicht <ipek.baris-schlicht@dw.com>
2024-04-02 14:10:59 +0200

    fix script call

--- scripts/scripts/retrieve_combinations.sh
+++ scripts/scripts/retrieve_combinations.sh
@@ -1,12 +1,11 @@
-#echo Generate Tag List With Attributes
-#python -m datageneration.retrieve_combinations \
-#--source datageneration/data/Primary_Keys_filtered8.xlsx \
-#--output_file datageneration/data/tag_combinations_v12.jsonl \
-#--generate_tag_list_with_attributes
+echo Generate Tag List With Attributes
+python -m datageneration.retrieve_combinations \
+--source datageneration/data/Primary_Keys_filtered9.xlsx \
+--output_file datageneration/data/tag_combinations_v12.jsonl \
+--generate_tag_list_with_attributes
 
 echo Generate Attribute Examples
 python -m datageneration.retrieve_combinations \
 --source datageneration/data/Primary_Keys_filtered9.xlsx \
 --output_file datageneration/data/att_examples_v12.jsonl \
---generate_tag_list_with_attributes
-#--generate_attribute_examples
+--generate_attribute_examples
